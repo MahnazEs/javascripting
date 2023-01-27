71793358fb6546be5cce9e8a0c4593a9d7780f0d
“MahnazEs” <esmaeilymahnaz@gmail.com>
2023-01-26 20:17:36 -0800

    object.js code

--- object.js
+++ object.js
@@ -1,4 +1,9 @@
-var food = {
-  types: 'only pizza'
-};
-console.log(food['types']);
+var pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+ 
+ 
+ };
+ 
+ console.log(pizza);
